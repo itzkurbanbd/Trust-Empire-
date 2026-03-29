0e3127c2be93c4f286b116a5dd39ae1e82728db3
itzkurbanbd <itzkurbanbd@gmail.com>
2026-03-29 14:34:27 +0600

    Create src/lib/firebase.js

---
+++ src/lib/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { firebaseConfig } from './firebaseConfig';
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, db, storage };
